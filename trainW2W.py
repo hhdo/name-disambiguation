5ababaa0ab63de50099dfd781d5b50d97c35cec9
donghcn <dg.hao@qq.com>
2020-12-22 16:11:55 +0800

    修正注释

--- trainW2W.py
+++ trainW2W.py
@@ -21,7 +21,7 @@ r = '[!“”"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~—～’]+'
 
 f1 = open ('gene/all_text.txt','w',encoding = 'utf-8')
 
-print('training train pub text:')
+print('reading train pub text:')
 for i,pid in enumerate(tqdm(pubs_raw)):
     pub = pubs_raw[pid]
     
@@ -66,7 +66,7 @@ for i,pid in enumerate(tqdm(pubs_raw)):
     pstr = re.sub(r'\s{2,}', ' ', pstr).strip()
     f1.write(pstr+'\n')
 
-print('training valid pub text:')
+print('reading valid pub text:')
 for i,pid in enumerate(tqdm(pubs_raw1)):
     pub = pubs_raw1[pid]
     
@@ -152,7 +152,7 @@ f1.close()
 
 
 from gensim.models import word2vec
-
+print('word2vec training...')
 sentences = word2vec.Text8Corpus(r'gene/all_text.txt')
 model = word2vec.Word2Vec(sentences, size=200,negative =10, min_count=2, window=4, iter=5,sg=1, hs=1, workers=50)
 model.save('word2vec/Aword2vec.model')
